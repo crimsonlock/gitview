243f5ad2df86904f7fcf98d08f5af7f48f3ba5bf
lishaopeng <shermy@163.com>
2018-12-29 12:40:44 +0800

    init

--- gitview.go
+++ gitview.go
@@ -4,20 +4,84 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"strings"
+	"regexp"
+	"strconv"
 )
 
 //get cat-file -p
 
 type object struct {
 	mod string //commit,tree,blob,tag
+	name string
 	hash string
 	children []*object
+	regCts [][]string
 	length int
 	level int
 }
 
-func readObjects(path string) []*object {
+/*
+func findChildren(obj *object, objs []*object){
+
+	switch obj.mod {
+
+	case "commit":
+		for _,o := range objs{
+			if obj.regCts[0][1] == o.hash {
+				obj.children = append(obj.children,o)
+				break
+			}
+		}
+	case "tree":
+
+
+	}
+
+}
+*/
+
+func setChildrenLevel(obj *object){
+
+	if len(obj.children)==0 {
+		return
+	}
+
+	i := obj.level-1
+	for _,chi := range obj.children {
+		chi.level = i
+		setChildrenLevel(chi)
+	}
+
+}
+
+func fixMap(objs map[string]*object){
+
+	for _,obj := range objs{
+
+		switch obj.mod {
+
+		case "commit":
+			obj.name = "[commit]"
+			obj.children = append(obj.children, objs[obj.regCts[0][1]])
+			objs[obj.regCts[0][1]    ].name = "[root]"
+		case "tree":
+			for _,al := range obj.regCts {
+				obj.children = append(obj.children,objs[al[2]])
+				objs[al[2]].name = al[3]
+			}
+		case "tag":
+
+		}
+
+
+	}
+
+}
+
+func readObjects(path string) map[string]*object {
+
+	//
+	objs := make(map[string]*object)
 
 	//Check git path exsit
 	_, err := os.Stat(path+"/.git")
@@ -36,21 +100,74 @@ func readObjects(path string) []*object {
 	cts := string(rs)
 
 	//Read each line and insert to array
-	lines := strings.Split(cts,"\n")
+	regLines,_:= regexp.Compile(`([0-9a-z]+)\s(commit|blob|tree|tag)\s([\d]+)\n`)
+	regCommit,_:= regexp.Compile(`tree\s([0-9a-z]+)\n`)
+	regTree,_ := regexp.Compile(`[\d]+\s(commit|blob|tree|tag)\s([0-9a-z]+)[\s]+([\S]+)\n`)
+	//regBlob,_ := regexp.Compile(``)
+	//regTag,_ := regexp.Compile(``)
+
+	lines := regLines.FindAllStringSubmatch(cts,-1)
+
 	for _,line := range lines{
-		wds := strings.Split(line," ")
-		info,_ := exec.Command("git","cat-file","-p",wds[0]).Output()
-		fmt.Printf("%s\n",info)
+
+		ot,_ := exec.Command("git","cat-file","-p",line[1]).Output()
+		content := string(ot)
+
+		switch line[2] {
+
+		case "commit":
+			i,_ := strconv.Atoi(line[3])
+			s := regCommit.FindAllStringSubmatch(content,-1)
+			obj := &object{
+				mod:"commit",
+				hash:line[1],
+				regCts:s,
+				length:i,
+			}
+			objs[line[1]]=obj
+
+		case "tree":
+			i,_ := strconv.Atoi(line[3])
+			s := regTree.FindAllStringSubmatch(content,-1)
+			obj := &object{
+				mod:"tree",
+				hash:line[1],
+				regCts:s,
+				length:i,
+			}
+			objs[line[1]]=obj
+		case "blob":
+			i,_ := strconv.Atoi(line[3])
+			obj := &object{
+				mod:"blob",
+				hash:line[1],
+				length:i,
+			}
+			objs[line[1]]=obj
+
+		case "tag":
+
+		}
+
+
 	}
 
-	return nil
+	return objs
 
 }
 
 func parseProject(path string) {
 
+	mp := readObjects(path)
+	fixMap(mp)
 
+	for _,m:=range mp{
+		setChildrenLevel(m)
+	}
 
+	for _,kl:=range mp{
+		fmt.Printf("%+v\n",kl)
+	}
 
 	//fis,_:=ioutil.ReadDir(path+"/.git/objects")
 
