c67bd3c051d504fae8f70aea1137e5eae397afd6
lishaopeng <shermy@163.com>
2019-01-02 12:00:12 +0800

    modify comments

--- gitview.go
+++ gitview.go
@@ -7,57 +7,38 @@ import (
 	"strconv"
 )
 
-//get cat-file -p
-
+//git object struct
 type object struct {
-	mod string //commit,tree,blob,tag
-	hash string //hash string
-	children []*object //child
-	regCts [][]string //regexp rs for text
-	length int //text length
-	level int //tree level
-	name string //display name when draw
-	top float64 //top px when draw
-	left float64 //left px when draw
+	mod      string     //commit,tree,blob,tag
+	hash     string     //hash string
+	children []*object  //child
+	regCts   [][]string //regexp rs for text
+	length   int        //text length
+	level    int        //tree level
+	name     string     //display name when draw
+	top      float64    //top px when draw
+	left     float64    //left px when draw
 }
 
-/*
-func findChildren(obj *object, objs []*object){
+//set object tree level in order for easy output
+func setChildrenLevel(obj *object) {
 
-	switch obj.mod {
-
-	case "commit":
-		for _,o := range objs{
-			if obj.regCts[0][1] == o.hash {
-				obj.children = append(obj.children,o)
-				break
-			}
-		}
-	case "tree":
-
-
-	}
-
-}
-*/
-
-func setChildrenLevel(obj *object){
-
-	if len(obj.children)==0 {
+	if len(obj.children) == 0 {
 		return
 	}
 
-	i := obj.level-1
-	for _,chi := range obj.children {
+	i := obj.level - 1
+	for _, chi := range obj.children {
 		chi.level = i
 		setChildrenLevel(chi)
 	}
 
 }
 
-func fixMap(objs map[string]*object){
+//set object display name
+func fixMap(objs map[string]*object) {
 
-	for _,obj := range objs{
+	for _, obj := range objs {
 
 		switch obj.mod {
 
@@ -66,115 +47,118 @@ func fixMap(objs map[string]*object){
 			obj.children = append(obj.children, objs[obj.regCts[0][1]])
 			objs[obj.regCts[0][1]].name = "[root]"
 		case "tree":
-			for _,al := range obj.regCts {
-				obj.children = append(obj.children,objs[al[2]])
+			for _, al := range obj.regCts {
+				obj.children = append(obj.children, objs[al[2]])
 				objs[al[2]].name = al[3]
 			}
 		case "tag":
 
 		}
 
-
 	}
 
 }
 
-func readObjects(path string) (map[string]*object,[]string) {
+//Read object from .git/objects
+//path: path to project
+//return-1: map hold all objects, key string hold hash value
+//return-2: hash value string slice, in order to get a certain sequnce
+func readObjects(path string) (map[string]*object, []string) {
 
-	//
+	//return values
 	objs := make(map[string]*object)
 	hashs := []string{}
 
 	//Check git path exsit
-
-	_, err := os.Stat(path+string(os.PathSeparator)+".git")
+	_, err := os.Stat(path + string(os.PathSeparator) + ".git")
 	if err != nil && os.IsNotExist(err) {
-		return nil,nil
+		return nil, nil
 	}
 
 	//Change dir for git commands
 	os.Chdir(path)
 
 	//Cat all obejcts
-	rs , err := exec.Command("git","cat-file","--batch-check","--batch-all-objects").Output()
-	if err !=  nil{
-		return nil,nil
+	rs, err := exec.Command("git", "cat-file", "--batch-check", "--batch-all-objects").Output()
+	if err != nil {
+		return nil, nil
 	}
 	cts := string(rs)
 
 	//Read each line and insert to array
-	regLines,_:= regexp.Compile(`([0-9a-z]+)\s(commit|blob|tree|tag)\s([\d]+)\n`)
-	regCommit,_:= regexp.Compile(`tree\s([0-9a-z]+)\n`)
-	regTree,_ := regexp.Compile(`[\d]+\s(commit|blob|tree|tag)\s([0-9a-z]+)[\s]+([\S]+)\n`)
-	//regBlob,_ := regexp.Compile(``)
-	//regTag,_ := regexp.Compile(``)
+	regLines, _ := regexp.Compile(`([0-9a-z]+)\s(commit|blob|tree|tag)\s([\d]+)\n`)
+	regCommit, _ := regexp.Compile(`tree\s([0-9a-z]+)\n`)
+	regTree, _ := regexp.Compile(`[\d]+\s(commit|blob|tree|tag)\s([0-9a-z]+)[\s]+([\S]+)\n`)
 
-	lines := regLines.FindAllStringSubmatch(cts,-1)
+	lines := regLines.FindAllStringSubmatch(cts, -1)
+	for _, line := range lines {
 
-	for _,line := range lines{
-
-		hashs = append(hashs,line[1])
-		ot,_ := exec.Command("git","cat-file","-p",line[1]).Output()
+		hashs = append(hashs, line[1])
+		ot, _ := exec.Command("git", "cat-file", "-p", line[1]).Output()
 		content := string(ot)
 
 		switch line[2] {
 
 		case "commit":
-			i,_ := strconv.Atoi(line[3])
-			s := regCommit.FindAllStringSubmatch(content,-1)
+			i, _ := strconv.Atoi(line[3])
+			s := regCommit.FindAllStringSubmatch(content, -1)
 			obj := &object{
-				mod:"commit",
-				hash:line[1],
-				regCts:s,
-				length:i,
+				mod:    "commit",
+				hash:   line[1],
+				regCts: s,
+				length: i,
 			}
-			objs[line[1]]=obj
+			objs[line[1]] = obj
 
 		case "tree":
-			i,_ := strconv.Atoi(line[3])
-			s := regTree.FindAllStringSubmatch(content,-1)
+			i, _ := strconv.Atoi(line[3])
+			s := regTree.FindAllStringSubmatch(content, -1)
 			obj := &object{
-				mod:"tree",
-				hash:line[1],
-				regCts:s,
-				length:i,
+				mod:    "tree",
+				hash:   line[1],
+				regCts: s,
+				length: i,
 			}
-			objs[line[1]]=obj
+			objs[line[1]] = obj
 		case "blob":
-			i,_ := strconv.Atoi(line[3])
+			i, _ := strconv.Atoi(line[3])
 			obj := &object{
-				mod:"blob",
-				hash:line[1],
-				length:i,
+				mod:    "blob",
+				hash:   line[1],
+				length: i,
 			}
-			objs[line[1]]=obj
+			objs[line[1]] = obj
 
 		case "tag":
 
 		}
 
-
 	}
 
-	return objs,hashs
+	return objs, hashs
 
 }
 
+//Parse project
+//path: path to project
+//return-1: map hold all objects, key string hold hash value
+//return-2: hash value string slice, in order to get a certain sequnce
+func parseProject(path string) (map[string]*object, []string) {
 
-func parseProject(path string) (map[string]*object,[]string){
-
-	mp,hashs := readObjects(path)
-	if mp==nil{
-		return nil,nil
+	//read objects
+	mp, hashs := readObjects(path)
+	if mp == nil {
+		return nil, nil
 	}
 
+	//set dispaly value
 	fixMap(mp)
-	for _,m:=range mp{
+
+	//set children level
+	for _, m := range mp {
 		setChildrenLevel(m)
 	}
 
-	return mp,hashs
+	return mp, hashs
 
 }
-
-
--- server.go
+++ server.go
@@ -14,7 +14,7 @@ func init() {
 
 func htmlProject(response http.ResponseWriter, request *http.Request) {
 
-
+	//return content head
 	cts := `<!DOCTYPE html>
 <html lang="zh-CN">
 <head>
@@ -31,59 +31,71 @@ p{margin:5px;padding:0px;text-align:center;font-size:14px}
 </style>
 </head>
 <body>`
+
+	//max level for display
 	stack := [10]int{}
 
+	//get real path
 	request.ParseForm()
 	path, ok := request.Form["path"]
-	pp:=""
+	pp := ""
 	if ok {
-		pp = strings.Replace(path[0],"|",string(os.PathSeparator),-1)
+		pp = strings.Replace(path[0], "|", string(os.PathSeparator), -1)
 	}
 
-	mp,hashs := parseProject(string(os.PathSeparator)+pp)
-	if mp == nil{
-		fmt.Fprintf(response,"%s<p>Bad project path : %c%s</p></body></html>",cts,os.PathSeparator,pp)
+	//read objects
+	mp, hashs := parseProject(string(os.PathSeparator) + pp)
+	if mp == nil {
+		fmt.Fprintf(response, "%s<p>Bad project path : %c%s</p></body></html>", cts, os.PathSeparator, pp)
 		return
 	}
 
-	for _,hs := range hashs{
+	//show objects rects
+	for _, hs := range hashs {
 		v := mp[hs]
-		v.top = float64(100-200*v.level)
-		v.left = float64(100+200*stack[-1*v.level])
+		v.top = float64(100 - 200*v.level)
+		v.left = float64(100 + 200*stack[-1*v.level])
 		stack[-1*v.level]++
-		cts = fmt.Sprintf("%s<div class='%s' style='top:%dpx;left:%dpx'><p>%s<br>%s</p></div>\n",cts,v.mod,int(v.top),int(v.left),v.hash[0:6],v.name)
+		cts = fmt.Sprintf("%s<div class='%s' style='top:%dpx;left:%dpx'><p>%s<br>%s</p></div>\n", cts, v.mod, int(v.top), int(v.left), v.hash[0:6], v.name)
 	}
 
-	for _,hs := range hashs{
+	//draw lines between rects
+	for _, hs := range hashs {
 		v := mp[hs]
-		for _,chd := range v.children{
-			cts = cts+drawLine(v,chd)
+		for _, chd := range v.children {
+			cts = cts + drawLine(v, chd)
 		}
 	}
 
-
-	fmt.Fprintf(response,"%s</body></html>",cts)
-
+	fmt.Fprintf(response, "%s</body></html>", cts)
 
 }
 
-func drawLine(o1 *object,o2 *object) string{
+//draw lines between o1 and o2
+func drawLine(o1 *object, o2 *object) string {
 
-
+	//line length
 	length := math.Sqrt(float64(22500 + (o1.left-o2.left)*(o1.left-o2.left)))
-	top := int((o1.top+o2.top)/2)+25
-	left := int((o1.left+o2.left-length)/2)+75
+
+	//line top
+	top := int((o1.top+o2.top)/2) + 25
+
+	//line left
+	left := int((o1.left+o2.left-length)/2) + 75
+
+	//line rotation
 	rot := 0.0
-	if o1.left == o2.left{
+	if o1.left == o2.left {
 		rot = 90.0
 	} else {
-		rot = 180.0 * math.Atan(-150/(o1.left-o2.left))/math.Pi
+		rot = 180.0 * math.Atan(-150/(o1.left-o2.left)) / math.Pi
 	}
-	return fmt.Sprintf("<div class='line' style='top:%dpx;left:%dpx;width:%dpx;-webkit-transform:rotate(%fdeg);'></div>\n",top,left,int(length),rot)
+	
+	return fmt.Sprintf("<div class='line' style='top:%dpx;left:%dpx;width:%dpx;-webkit-transform:rotate(%fdeg);'></div>\n", top, left, int(length), rot)
 }
 
-func main(){
+func main() {
 
-	http.ListenAndServe(":8802",nil)
+	http.ListenAndServe(":8802", nil)
 
 }
