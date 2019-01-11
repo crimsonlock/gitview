1f5b60ca1699bd8554b1678b839c50b83ab4730e
lishaopeng <shermy@163.com>
2019-01-11 21:09:35 +0800

    modify readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # gitview
+
+功能：
+读取 .git/objects  和  .git/refs/head 下边的内容
+以可视化的方式展示 git objects 的有向无环图
+
+使用：
+编译之后，运行，然后打开浏览器输入 http://127.0.0.1:8082/
+按照提示操作
+
+参考：搜索 "git pro"
--- gitview_test.go
+++ gitview_test.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestParseTree(t *testing.T) {
 
-	//parseProject("/Users/lishaopeng/go/src/github.com/crimsonlock/goinsight")
+	parseProject("/Users/lishaopeng/go/src/github.com/crimsonlock/goinsight")
 
 }
 
-func TestMath(t *testing.T) {
-	rot := (280 - 100) / (400 - 350)
-	fmt.Println(rot)
-}
